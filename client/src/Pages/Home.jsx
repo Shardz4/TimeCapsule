41e7a5d0f7b3e5bd4e4912c49fc21a8eb499d161
Sarthak Katiyar <156002273+Sarthak1970@users.noreply.github.com>
2025-02-22 11:50:08 +0530

    frontend and sentiment

---
+++ client/src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React,{useState} from 'react';
+import CapsuleForm from '../Components/CapsuleForm';
+import CapsuleList from '../Components/CapsuleList';
+
+const Home = () => {
+    return (
+        <>
+        <CapsuleForm />
+      </>
+    );
+  };
+  
+export default Home;
+  
