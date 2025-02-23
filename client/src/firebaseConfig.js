d4274dc55f21b974c9d2d4c9a502368e339d97b1
Harsh Saini <harshsaini0107@gmail.com>
2025-02-23 14:16:07 +0530

    firebase

---
+++ client/src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAEghlbixXJ94oUqBO5vopu14Dewa9lriE",
+  authDomain: "timecaps-92577.firebaseapp.com",
+  projectId: "timecaps-92577",
+  storageBucket: "timecaps-92577.firebasestorage.app",
+  messagingSenderId: "653028451486",
+  appId: "1:653028451486:web:9546f2e7885a52f5d6078f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
